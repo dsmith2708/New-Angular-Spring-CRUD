4b4a8f6b133c6da8e159d7349a6178e750ee5894
Daniel Smith <daniel.smith@academytrainee.com>
2018-06-22 08:52:48 +0100

    initial commit, blank spring project added

---
+++ server-side/angular-spring-crud/src/main/java/com/qa/angularspringcrud/AngularSpringCrudApplication.java
@@ -0,0 +1,12 @@
+package com.qa.angularspringcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularSpringCrudApplication.class, args);
+	}
+}
